5c314720e884c7a60483a951462a7074b1e4ca8b
unknown <techgeek01help@gmail.com>
2013-01-12 14:28:16 -0600

    Tweak

--- jclock.js
+++ jclock.js
@@ -10,10 +10,21 @@ function startTime(){
   m=checkTime(m);
   s=checkTime(s);
   document.getElementById('24clock').innerHTML=h24+":"+m+":"+s;
+  replaceContentInContainer('clock', h24+":"+m+":"+s);
   state=h12>11&&h12<24?"PM":"AM";
   h12=h12>12?h12-12:h12;
   document.getElementById('12clock').innerHTML=h12+":"+m+":"+s+" "+state;
   t=setTimeout(function(){startTime()},500);
+}
+
+function replaceContentInContainer(matchClass,content) {
+    var elems = document.getElementsByTagName('*'), i;
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+                > -1) {
+            elems[i].innerHTML = content;
+        }
+    }
 }
 
 function checkTime(i){
