99edd26af32f73200d0951163586beef436d1e6f
Andrew Yasger <techgeek01help@gmail.com>
2013-01-11 20:05:36 -0600

    Update handler.js
    Removed problematic 12 hour clock

--- handler.js
+++ handler.js
@@ -1,24 +1,18 @@
 function startTime(){
   var today=new Date();
-  var htwentyfour=today.getHours();
-  var htwelve=today.getHours();
+  var h=today.getHours();
   var m=today.getMinutes();
   var s=today.getSeconds();
+  // add a zero in front of numbers<10
   m=checkTime(m);
   s=checkTime(s);
-  document.getElementById('24clock').innerHTML=htwentyfour+":"+m+":"+s;
-  var mnorev="A";
-  if (htwelve>13){
-    var htwelve=htwelve-12;
-    var mnorev="P";
-  }
-  document.getElementById('12clock').innerHTML=htwelve+":"+m+":"+s+" "+mnorev+"M";
+  document.getElementById('24clock').innerHTML=h+":"+m+":"+s;
   t=setTimeout(function(){startTime()},500);
 }
 
 function checkTime(i){
   if (i<10){
-    1="0"+i;
+    i="0" + i;
   }
   return i;
 }
