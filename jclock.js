fc5ff1372f0e44c7cdfb9559859ed180bb2de7e1
TechGeek01 <techgeek01help@gmail.com>
2013-01-12 11:24:29 -0600

    Added 12-hour clock
    Removed unnecessary code, and reintroduced 12-hour clock.

--- jclock.js
+++ jclock.js
@@ -3,19 +3,24 @@ function startTime(){
   var year=today.getFullYear();
   var month=today.getMonth()+1;
   var date=today.getDate();
-  var year=today.getYear();
   var h24=today.getHours();
   var h12=today.getHours();
   var m=today.getMinutes();
   var s=today.getSeconds();
+  var state="AM";
   m=checkTime(m);
   s=checkTime(s);
   document.getElementById('24clock').innerHTML=h24+":"+m+":"+s;
+  if(h12>12){
+    var h12=h12-12;
+    var state="PM";
+  }
+  document.getElementById('12clock').innerHTML=h12+":"+m+":"+s+" "+state;
   t=setTimeout(function(){startTime()},500);
 }
 
 function checkTime(i){
-  if (i<10){
+  if(i<10){
     i="0"+i;
   }
   return i;
