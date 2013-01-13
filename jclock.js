fa55d42cea7bd73512642ced180dc65ecb7c60c5
Andrew Yasger <techgeek01help@gmail.com>
2013-01-13 16:00:08 -0600

    Code formatting fix

--- jclock.js
+++ jclock.js
@@ -1,22 +1,24 @@
-function startTime(){
-  var today=new Date();
-  var year=today.getFullYear();
-  var month=today.getMonth()+1;
-  var date=today.getDate();
-  var h24=today.getHours();
-  var h12=today.getHours();
-  var m=checkTime(today.getMinutes());
-  var s=checkTime(today.getSeconds());
-  var state="AM";
-  if(h12>11&&h12<24){
-    state="PM";
+function startTime() {
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  var date = today.getDate();
+  var h24 = today.getHours();
+  var h12 = today.getHours();
+  var m = checkTime(today.getMinutes());
+  var s = checkTime(today.getSeconds());
+  var state = "AM";
+  if (h12 > 11 && h12 < 24) {
+    state = "PM";
   }
-  if(h12>12){
-    h12=h12-12;
+  if (h12 > 12) {
+    h12 = h12 - 12;
   }
-  replaceContent("24clock",h24+":"+m+":"+s);
-  replaceContent("12clock",h12+":"+m+":"+s+" "+state);
-  t=setTimeout(function(){startTime()},500);
+  replaceContent("24clock", h24 + ":" + m + ":" + s);
+  replaceContent("12clock", h12 + ":" + m + ":" + s + " " + state);
+  t = setTimeout(function () {
+    startTime();
+  }, 500);
 }
 
 /*
@@ -34,18 +36,19 @@ function clockType(i){
 }
 */
 
-function replaceContent(matchClass,content){
-  var elems=document.getElementsByTagName('*'),i;
-  for(i in elems){
-    if((" "+elems[i].className+" ").indexOf(" "+matchClass+" ")>-1){
-      elems[i].innerHTML=content;
+function replaceContent(matchClass, content) {
+  var elems = document.getElementsByTagName('*'),
+    i;
+  for (i in elems) {
+    if ((" " + elems[i].className + " ").indexOf(" " + matchClass + " ") > -1) {
+      elems[i].innerHTML = content;
     }
   }
 }
 
-function checkTime(i){
-  if(i<10){
-    i="0"+i;
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
   }
   return i;
 }
