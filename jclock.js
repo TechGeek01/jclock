2cdfea3f3bec051259bc0ca7ee54065258ba0516
Andrew Yasger <techgeek01help@gmail.com>
2013-02-20 19:57:03 -0600

    Revert back to revision 0

--- jclock.js
+++ jclock.js
@@ -1,5 +1,5 @@
 /***************************
-*   jClock v1.1.0 rev. 4   *
+*   jClock v1.0.10 rev. 5  *
 *          2-20-13         *
 ***************************/
 
@@ -8,19 +8,19 @@
 function initialSet(){
   //Currently deprecated, though this may return in a later version
   /*
-  //Append footer information to page
-  try {
-    var jClockFooter = document.createElement("p");
-    jClockFooter.id = "jclockfooter";
-    document.getElementById("footer").appendChild(jClockFooter);
-  } catch (err){
-    var jClockFooter = document.createElement("div");
-    jClockFooter.id = "jclockfooter";
-    document.body.appendChild(jClockFooter);
-    document.getElementById("jclockfooter").style.cssText = "font-size:12px;font-family:Arial;text-align:center;";
-  }
-  document.getElementById("jclockfooter").innerHTML = "<a href='http://techgeek01.github.com/jclock' target='_blank'>jClock</a> v1.1.0";
-  */
+//Append footer information to page
+try {
+var jClockFooter = document.createElement("p");
+jClockFooter.id = "jclockfooter";
+document.getElementById("footer").appendChild(jClockFooter);
+} catch (err){
+var jClockFooter = document.createElement("div");
+jClockFooter.id = "jclockfooter";
+document.body.appendChild(jClockFooter);
+document.getElementById("jclockfooter").style.cssText = "font-size:12px;font-family:Arial;text-align:center;";
+}
+document.getElementById("jclockfooter").innerHTML = "<a href='http://techgeek01.github.com/jclock' target='_blank'>jClock</a> v1.0.8";
+*/
 }
 
 (function(){
@@ -39,7 +39,7 @@ function initialSet(){
     var year = today.getFullYear();
     var month = today.getMonth()+1;
     var date = today.getDate();
-	
+
     var h24 = today.getHours();
     var h12 = today.getHours();
     var m = checkTime(today.getMinutes());
@@ -47,13 +47,13 @@ function initialSet(){
     if (h12 > 11){
       var state = "PM";
     } else {
-	  var state = "AM";
-	}
+var state = "AM";
+}
     if (h12 > 12){
       h12 = h12 - 12;
     }
-	
-	//If any floating clocks exist, style the first one by priority
+
+//If any floating clocks exist, style the first one by priority
     try {
       document.getElementById("float12clockdark").innerHTML = h12 + ":" + m + " " + state;
       floatStyle("float12clockdark");
@@ -73,9 +73,9 @@ function initialSet(){
         }
       }
     }
-	
-	replaceContent("\x6A\x63\x6C\x6F\x63\x65\x61\x73\x74\x65\x72\x65\x67\x67","\x6A\x43\x6C\x6F\x63\x6B\x20\x69\x73\x20\x61\x77\x65\x73\x6F\x6D\x65\x21");
-	//replaceContent("\x65\x61\x73\x74\x65\x72\x65\x67\x67","&#106;&#67;&#108;&#111;&#99;&#107;&#32;&#105;&#115;&#32;&#97;&#119;&#101;&#115;&#111;&#109;&#101;&#33;");
+
+replaceContent("\x6A\x63\x6C\x6F\x63\x65\x61\x73\x74\x65\x72\x65\x67\x67","\x6A\x43\x6C\x6F\x63\x6B\x20\x69\x73\x20\x61\x77\x65\x73\x6F\x6D\x65\x21");
+//replaceContent("\x65\x61\x73\x74\x65\x72\x65\x67\x67","&#106;&#67;&#108;&#111;&#99;&#107;&#32;&#105;&#115;&#32;&#97;&#119;&#101;&#115;&#111;&#109;&#101;&#33;");
     replaceContent("24clock",h24 + ":" + m +":" + s);
     replaceContent("12clock",h12 + ":" + m + ":" + s + " " + state);
     t = setTimeout(function (){startTime();},500);
@@ -112,12 +112,11 @@ function initialSet(){
   function floatStyle(i){
     var verticalFloatPos = viewport("height")-59;
     verticalFloatPos = verticalFloatPos.toString();
-	horizontalFloatPos = 0;
     if (i === "float24clock" || i === "float12clock"){
       var clockColor = "ffffff";
     } else {
-	  var clockColor = "000000";
-	}
-      document.getElementById(i).style.cssText = "font-size:50px;color:#" + clockColor + ";font-family:arial;position:fixed;top:" + verticalFloatPos + "px;left:10px;z-index:200;";
+var clockColor = "000000";
+}
+    document.getElementById(i).style.cssText = "font-size:50px;color:#" + clockColor + ";font-family:arial;position:fixed;top:" + verticalFloatPos + "px;left:10px;z-index:200;";
   }
 }());
